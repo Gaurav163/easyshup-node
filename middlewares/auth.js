a6a8fd931b3c5bb99fbaf935410b570601d0c581
Gaurav163 <gouravlathwal63@gmail.com>
2021-10-07 15:08:52 +0530

    wow

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,9 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers["x-access-token"] || req.headers["x-auth-token"];
         if (!token) {
-            console.log(req.headers);
             res.status(403).json({ message: "Please Sign In" });
         }
         else {
-            console.log(token);
             const decodedToken = jwt.verify(token, process.env.JWT_secret_token);
             req.user = decodedToken;
             next();
--- routes/seller.js
+++ routes/seller.js
@@ -5,19 +5,37 @@ const Product = require("../models/product");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
+router.route("/myproducts").get([auth], async (req, res) => {
+    try {
+        if (req.user.type !== 'Seller') {
+            res.status(403).send("Only Sellers Can Add Products!")
+        } else {
+            const myproducts = await Product.find({ seller: req.user.email });
+            res.status(200).send({ myproducts });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Something Went Wrong!");
+    }
+})
+
 
 
 
 router.route("/addproduct").post([auth], async (req, res) => {
     try {
-        let product = req.body;
-        product.email = req.user.email;
-        product.shop = req.user.shop;
-        product.tags = [];
-        product.description = "";
-        const newProduct = new Product(product);
-        await newProduct.save();
-        res.status(200).json({ Product: newProduct });
+        if (req.user.type !== 'Seller') {
+            res.status(403).send("Only Sellers Can Add Products!")
+        } else {
+            let product = req.body;
+            product.email = req.user.email;
+            product.shop = req.user.shop;
+            product.tags = [];
+            product.description = "";
+            const newProduct = new Product(product);
+            await newProduct.save();
+            res.status(200).json({ Product: newProduct });
+        }
     }
     catch (err) {
         console.log(err);
